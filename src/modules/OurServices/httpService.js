e26edfd0a22d4fbb8eee325aa400e6177861de2d
Artyom Pogosov <artyomp@outlook.com>
2020-03-14 16:26:47 +0200

    first commit

--- src/modules/OurServices/httpService.js
+++ src/modules/OurServices/httpService.js
@@ -3,8 +3,8 @@ import React from 'react';
 import axios from 'axios';
 
 const BASE_URL = process.env.NODE_ENV === 'production' ?
-    '/api/' :
-    '//localhost:3030/api/';
+    '/' :
+    '//localhost:3030/';
 
 
 // import Axios from 'axios';
